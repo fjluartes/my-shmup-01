605e9760f660121ccbd1de8ea00048333f5e1acf
Fred W11 <fjluartes@gmail.com>
2026-06-28 17:28:50 +0800

    Edits defs, structs

--- src/structs.h
+++ src/structs.h
@@ -4,12 +4,20 @@
 typedef struct Entity Entity;
 typedef struct Explosion Explosion;
 typedef struct Debris Debris;
+typedef struct Texture Texture;
 
 typedef struct 
 {
 	void (*logic)(void);
 	void (*draw)(void);
 } Delegate;
+
+struct Texture
+{
+	char name[MAX_NAME_LENGTH];
+	SDL_Texture *texture;
+	Texture *next;
+};
 
 typedef struct
 {
@@ -63,6 +71,7 @@ typedef struct
 {
 	Entity fighterHead, *fighterTail;
 	Entity bulletHead, *bulletTail;
+	Entity pointsHead, *pointsTail;
 	Explosion explosionHead, *explosionTail;
 	Debris debrisHead, *debrisTail;
 	int score;
@@ -74,3 +83,15 @@ typedef struct
 	int y;
 	int speed;
 } Star;
+
+typedef struct
+{
+	char name[MAX_SCORE_NAME_LENGTH];
+	int recent;
+	int score;
+} Highscore;
+
+typedef struct 
+{
+	Highscore highscore[NUM_HIGHSCORES];
+} Highscores;
