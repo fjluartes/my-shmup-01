5c9e080f8eb1ccb37f56bf2538146fe8292231f3
Fred W11 <fjluartes@gmail.com>
2026-06-28 17:23:15 +0800

    Adds sound.c

---
+++ src/sound.c
@@ -0,0 +1,32 @@
+/*
+ * sound.c: File to play sounds and music
+ */
+#include <SDL_mixer.h>
+
+#include "common.h"
+
+#include "sound.h"
+
+extern App app;
+
+static void loadSounds(void);
+
+void initSounds(void)
+{
+
+}
+
+void loadMusic(char *filename)
+{
+
+}
+
+void playMusic(int loop)
+{
+
+}
+
+static void loadSounds(void)
+{
+
+}
